0554656f27b4af7c17c1d5639ddbfd4f630ec1bc
Sutanu Sain <sutanu@lapn222398.galaxy.radixweb.com>
2022-11-03 18:28:50 +0530

    new1

---
+++ src/services/hostnameServerApi.js
@@ -0,0 +1,8 @@
+
+ export const login = "http://localhost:5000/api/v1";
+// export const envConfig = "envConfig";
+// export const user = "user";
+// export const hostname = "5000";
+// export const port = "port";
+// export const cloudUrl = "cloudUrl";
+// export const login = "login";
